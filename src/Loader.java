ae89a639f69ff52620b3ee315db724a80865ad16
Yereke <bikeyev@gmail.com>
2022-02-23 16:04:51 +0600

    first

---
+++ src/Loader.java
@@ -0,0 +1,14 @@
+public class Loader {
+    public static void main(String[] args) {
+        String str = "Hi!";
+        Dog dog1 = new Dog();
+        System.out.printf("Вес кошки N1: %.2f %n", dog1.getWeight());
+        dog1.feed(200.0);
+        dog1.woof();
+        System.out.printf("Вес кошки N1 после кормежки: %.2f %n", dog1.getWeight());
+        System.out.printf("Вес крмежки: %.2f %n", dog1.getWeight());
+
+
+    }
+
+    }
